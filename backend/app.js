a981ced97a3eb22ff2840f1afc55c9f8254e49c8
Dhruv-decoders <dhruv.b@decodershq.com>
2025-10-02 10:02:51 +0530

    dd

--- backend/app.js
+++ backend/app.js
@@ -21,37 +21,37 @@ const allowedOrigins = [
   "https://ecommerce-mern-pkl1.onrender.com", // production
 ];
 
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       if (!origin) return callback(null, true);
-//       if (!allowedOrigins.includes(origin)) {
-//         return callback(
-//           new Error("CORS policy does not allow access from this origin."),
-//           false
-//         );
-//       }
-//       return callback(null, true);
-//     },
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (!allowedOrigins.includes(origin)) {
+        return callback(
+          new Error("CORS policy does not allow access from this origin."),
+          false
+        );
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
 
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-// app.use(
-//   helmet({
-//     contentSecurityPolicy: {
-//       useDefaults: true,
-//       directives: {
-//         "connect-src": ["'self'", "https://api.stripe.com"],
-//         "img-src": ["'self'", "https://*.stripe.com"],
-//         "script-src": ["'self'", "https://js.stripe.com"],
-//       },
-//     },
-//   })
-// );
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      useDefaults: true,
+      directives: {
+        "connect-src": ["'self'", "https://api.stripe.com"],
+        "img-src": ["'self'", "https://*.stripe.com"],
+        "script-src": ["'self'", "https://js.stripe.com"],
+      },
+    },
+  })
+);
 
 // API routes
 app.use("/api/v2/auth", authRoutes);
@@ -62,14 +62,16 @@ app.use("/api/v2/payments", paymentRoute);
 app.use("/api/v2/analytics", analyticsRoutes);
 
 // Serve frontend in production
+// Serve frontend in production
 if (process.env.NODE_ENV === "production") {
   const frontendPath = path.join(__dirname, "frontend", "dist");
   app.use(express.static(frontendPath));
 
-  // // React SPA catch-all
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.join(frontendPath, "index.html"));
-  // });
+  // React SPA catch-all (very important for React Router)
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
 }
+
 
 export default app;
